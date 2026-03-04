e47132e9058c3963710ae6d1a79edde9450aca60
Tigran Petrosyan <tigr-080807@mail.ru>
2026-03-04 22:47:28 +0500

    Создание модели тренировки

---
+++ apps/exercise/models.py
@@ -0,0 +1,30 @@
+import uuid
+
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, editable=True, unique=True, default=uuid.uuid4)
+    name = models.CharField(max_length=25, verbose_name='Name of category')
+    description = models.TextField(verbose_name='Description of category')
+
+
+class Exercise(models.Model):
+    id = models.UUIDField(primary_key=True, editable=True, unique=True, default=uuid.uuid4)
+    name = models.CharField(max_length=50, verbose_name='Name of exercise')
+    description = models.TextField(verbose_name='Description of exercise')
+    icon = models.ImageField(verbose_name='Icon of exercise', upload_to='exercise_icon')
+    repeat = models.IntegerField(verbose_name='Count of repeat', default=0)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='exercises',
+        verbose_name='Category of exercise'
+    )
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [models.Index(fields=('name', 'category'))]
